379b33a4f88d474db70c45a08ddc084b8ab3667f
Stefan Fodor <xtephan@gmail.com>
2014-06-06 13:59:30 +0200

    saving in DB

--- tools/log_error.php
+++ tools/log_error.php
@@ -7,6 +7,40 @@
  */
 defined('C5_EXECUTE') or die(_("Access Denied."));
 
-echo "<pre>";
+if( $_GET["debug"] == "1" ) {
+	echo "<pre>";
+	print_r($_GET);
+}
 
-print_r($_POST);
+$place = sprintf( "File: %s on line %s, char %s", $_GET["file"],  $_GET["lineNumber"],  $_GET["columnNumber"] ); 
+$url = sprintf( "URL: %s", $_GET["url"] );
+
+$ua = sprintf("User Agent: %s", $_GET["userAgent"] );
+
+//Open the log stream
+$l = new Log('JS ' . $_GET["name"], true);
+
+$l->write( $_GET["message"] );
+
+$l->write(' ');
+
+$l->write($place);
+$l->write($url);
+
+$l->write(' ');
+
+$l->write($ua);
+
+$l->write(' ');
+
+$l->write( $_GET["stacktrace"] );
+
+//close and save
+$l->close();
+
+//output a 1x1 transparent pixel
+if( $_GET["debug"] != "1" ) {
+	header('Content-Type: image/gif');
+	//equivalent to readfile('pixel.gif')
+	echo "\x47\x49\x46\x38\x37\x61\x1\x0\x1\x0\x80\x0\x0\xfc\x6a\x6c\x0\x0\x0\x2c\x0\x0\x0\x0\x1\x0\x1\x0\x0\x2\x2\x44\x1\x0\x3b";
+}
